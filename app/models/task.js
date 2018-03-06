b59cae5213527d6de7bc132a067ab5fb0d7ff7f7
wbounas <wbounas@gmail.com>
2018-03-06 10:59:02 -0500

    Write `task` model, `tasks` template and route
    -- Currently, `task` model serializes all data from API
       - CONFIRMED: user_id is properly set, displaying only user owned data
    -- Modify `tasks.js` route handler to findAll('task') from 'store'
    -- Wrote basic tasks template to display all tasks
       - Currently using a <ul> to display all tasks

--- app/models/task.js
+++ app/models/task.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr('string'),
+  priority: DS.attr('number'),
+  difficulty: DS.attr('string'),
+  description: DS.attr('string'),
+  est_time: DS.attr('number'),
+  req_tools: DS.attr('string'),
+  notes: DS.attr('string'),
+  completed: DS.attr('boolean'),
+  user: DS.belongsTo('user')
 });
