24a10832e4b4d65d25bfa43f27615437744392d9
wbounas <wbounas@gmail.com>
2018-03-06 14:08:18 -0500

    Implement `task-viewer` component with basic styling
    -- Wrote component controller code for `task-viewer`
       - Add HTML class `task-view` for styling component and children
    -- Add basic styling in `app/styles/app.scss`
    -- Add basic component template that displays task name
    
    Note: `app/styles/task-viewer.scss` is not being recognized with live reload

--- app/components/task-viewer.js
+++ app/components/task-viewer.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['task-view'],
+  editing: false,
+  actions: {
+    toggleEditing () {
+      this.toggleProperty('editing')
+    }
+  }
 });
