ff334787ffabc11e5c6fdc118195c2992e4d105b
wbounas <wbounas@gmail.com>
2018-03-06 13:53:42 -0500

    Implement basic `task` route
    -- Define `task` model hook
       - Returns record from Ember data-store that matches params.task_id
    -- Wrote basic `task` template to show `task.name`
    -- Modify `/tasks` template
       - Add {{link-to}} helpers for each task that links to new `task` route

--- app/routes/task.js
+++ app/routes/task.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model (params) {
+    const id = params.task_id
+    return this.get('store').findRecord('task', id)
+  }
 });
