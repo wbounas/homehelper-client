b64d0b422d4fe025d46c23206fd5c7d503b1776a
wbounas <wbounas@gmail.com>
2018-03-06 11:22:22 -0500

    Write route handler for tasks/new, update templates
    -- Route handler for `tasks/new` currently saves name of task
    -- `tasks/` route now has necessary {{outlet}} to render child templates
    -- Currently, `tasks/new` only has an input field and a save button
       - `tasks/new` is set up to only save task name currently

--- app/routes/tasks/new.js
+++ app/routes/tasks/new.js
@@ -1,5 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-
+  model () {
+    return this.get('store').createRecord('task', {})
+  },
+  actions: {
+    save (newTask) {
+      console.log('please run save');
+      console.log('newTask is:', newTask);
+      const taskName = newTask.get('name')
+      console.log('taskName is:', taskName);
+      return newTask.save()
+        .then(() => this.transitionTo('tasks'))
+    }
+  }
 });
