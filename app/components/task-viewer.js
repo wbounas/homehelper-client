fff9fd8e748db659fa171258f221c3714e6c7dce
wbounas <wbounas@gmail.com>
2018-03-06 15:19:12 -0500

    Implement feature to save notes on task
    -- Passes action (and data) up from task-viewer component to `task` route handler
    -- Task Route Handler persists data to Ember-DS and API
    
    NOTE: Temporary feature (proof of concept)

--- app/components/task-viewer.js
+++ app/components/task-viewer.js
@@ -3,7 +3,18 @@ import Component from '@ember/component';
 export default Component.extend({
   classNames: ['task-view'],
   editing: false,
+
+  newNote: {
+    // new notes will be inserted here
+  },
+
   actions: {
+    save () {
+      const newThang = this.get('newNote')
+      newThang.task = this.get('task')
+      console.log('inside task-viewer save, newThang is:', newThang);
+      this.sendAction('saveNotes', newThang)
+    },
     toggleEditing () {
       this.toggleProperty('editing')
     }
--- app/routes/task.js
+++ app/routes/task.js
@@ -4,5 +4,25 @@ export default Route.extend({
   model (params) {
     const id = params.task_id
     return this.get('store').findRecord('task', id)
+  },
+  actions: {
+    saveNotes (newThang) {
+      console.log('inside task route handler saveNotes, newThang is:', newThang);
+      console.log('newThang.task.id is:', newThang.task.id);
+      const taskID = newThang.task.id
+      const newThangNotes = newThang.notes
+      const currentTask = this.get('store').findRecord('task', taskID)
+      console.log('currentTask is:', currentTask);
+      this.get('store').findRecord('task', taskID).then((task) => {
+        task.set('notes', newThangNotes)
+        task.save()
+      })
+
+      // NOTE: this doesn't work. why???
+      // NOTE: error says `cannot delegate set`
+      // currentTask.set('notes', newThangNotes)
+      // currentTask.save()
+      console.log('newerTask is:', currentTask);
+    }
   }
 });
