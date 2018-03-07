a8090f541539eeb476153b942dcf549d5b5fd930
wbounas <wbounas@gmail.com>
2018-03-07 12:38:46 -0500

    Add delete feature to `tasks` route
    -- Copied `deleteTask` action into `tasks` route
    -- Added a pencil on `tasks` route for users
       - Really a secondary method to transition to `task` route, meant to pair
         with trashcan icon

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -3,5 +3,15 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model () {
     return this.get('store').findAll('task')
+  },
+  actions: {
+    move (task) {
+      return this.transitionTo('task', task)
+    },
+    deleteTask (task) {
+      console.log('does deleteTask work?');
+      return task.destroyRecord()
+        .then(() => this.transitionTo('tasks'))
+    }
   }
 });
