0411d21a81697158d1b5af78e792e8fa97c2ad97
wbounas <wbounas@gmail.com>
2018-03-08 14:05:49 -0500

    Define Route Handler for `room`, add simple template and `rooms` link in nav
    -- Use code from `task` Route Handler for `room` Route Handler
    -- Add {{link-to}} `rooms` in nav if auth === true
    -- Add {{link-to 'room' room}} inside `/rooms` template

--- app/routes/room.js
+++ app/routes/room.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model (params) {
+    const id = params.task_id
+    return this.get('store').findRecord('room', id)
+  },
+  actions: {
+    deleteRoom (room) {
+      console.log('does deleteRoom work?');
+      return room.destroyRecord()
+        .then(() => this.transitionTo('rooms'))
+    },
+    saveNotesRoom (updatedRoom) {
+      const roomID = updatedRoom.room.get('id')
+      delete updatedRoom.room
+      const clone = Object.assign({}, updatedRoom)
+      this.get('store').findRecord('room', roomID).then((room) => {
+        room.setProperties(clone)
+        room.save()
+      })
+    }
+  }
 });
