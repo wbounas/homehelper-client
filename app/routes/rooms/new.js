e8085346dac7401402e64aa7d4c210a99ca5978f
wbounas <wbounas@gmail.com>
2018-03-08 13:52:40 -0500

    Wrote actions for `/rooms/new`, add buttons
    -- Added {{link-to}} helper in `/rooms` template
       - Used to access `/rooms/new`
    -- Added simple input field for saving new room name in `/rooms/new`

--- app/routes/rooms/new.js
+++ app/routes/rooms/new.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model () {
+    return this.get('store').createRecord('room', {});
+  },
+  actions: {
+    save (newRoom) {
+      // newRoom is passed in from save action in template
+      const roomName = newRoom.get('name');
+      console.log('roomName is:', roomName);
+      return newRoom.save()
+        .then(() => this.transitionTo('rooms'))
+    },
+    deleteRoom (newRoom) {
+      const roomName = newRoom.get('name');
+      return newRoom.destroyRecord('room', newRoom)
+        .then(() => this.transitionTo('rooms'));
+    }
+  }
 });
