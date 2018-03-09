15fd9fb6f801a733e14119d345293169b4e0ac9f
wbounas <wbounas@gmail.com>
2018-03-09 01:01:32 -0500

    Add error messaging to `rooms/new` route

--- app/routes/rooms/new.js
+++ app/routes/rooms/new.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  flashMessages: service(),
+
   model () {
     return this.get('store').createRecord('room', {});
   },
@@ -17,8 +20,21 @@ export default Route.extend({
       // newRoom is passed in from save action in template
       const roomName = newRoom.get('name');
       console.log('roomName is:', roomName);
-      return newRoom.save()
-        .then(() => this.transitionTo('rooms'))
+      if (roomName === undefined) {
+        this.get('flashMessages')
+        .danger('You need to insert a room name.')
+      } else {
+        return newRoom.save()
+          .then(() => {
+            this.get('flashMessages')
+            .success('Room Created')
+          })
+          .then(() => this.transitionTo('rooms'))
+          .catch(() => {
+            this.get('flashMessages')
+            .danger('There was a problem. Please try again.')
+          })
+      }
     },
     deleteRoom (newRoom) {
       const roomName = newRoom.get('name');
